6860009bfbcf14757a20fbc3015c5ce816e07f08
kennedy <dkennyvs@gmail.com>
2022-06-11 10:28:21 -0300

    NodeJS API Updated

--- src/index.js
+++ src/index.js
@@ -4,8 +4,24 @@ const express = require('express');
 const app = express();
 
 // localhost:3333
-app.get('/', (request, response) => {
-    return response.json({message: 'Hello World Ignite - Fundamentos NodeJS'});
+app.get('/courses', (request, response) => {
+        return response.json(['Curso 1', 'Curso 2', "Curso 3", 'Curso 4']);
+});
+
+app.post('/courses', (request, response) => {
+    return response.json(['Curso 1', 'Curso 2', "Curso 3", 'Curso 4']);
+});
+
+app.put('/courses/:id', (request, response) => {
+    return response.json(['Curso 666', 'Curso 2', "Curso 3", 'Curso 4']);
+});
+
+app.patch('/courses/:id', (request, response) => {
+    return response.json(['Curso 1', 'Curso 666', 'Curso 3', 'Curso 4']);
+});
+
+app.delete('/courses/:id', (request, response) => {
+    return response.json(['Curso 3'])
 });
 
 app.listen(3333);
