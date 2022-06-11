56c8714c1a2179f029066830b20d639132452b45
kennedy <dkennyvs@gmail.com>
2022-06-11 10:55:34 -0300

    NodeJS API Updated

---
+++ Readme_comentarios.md
@@ -0,0 +1,14 @@
+
+<h2>Métodos mais usados em ua API Rest</h2>
+
+- GET - Buscar uma informação dentro do servidor<br>
+- POST - Inserir uma informação no servidor<br>
+- PUT - Alterar uma informação no servidor<br>
+- PATCH - Alterar uma informação específica<br>
+- DELETE - Deletar uma informação no servidor<br>
+
+<h2>Tipos de PARÂMETROS</h2>
+
+- Route Params - Identificar um recurso, editar/deletar/buscar 
+- Query Params - Paginação/filtro
+- Body Params - Os objetos inserção/alteração (JSON)
--- src/index.js
+++ src/index.js
@@ -3,16 +3,24 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 // localhost:3333
 app.get('/courses', (request, response) => {
+    const query = request.query;
+    console.log(query);
         return response.json(['Curso 1', 'Curso 2', "Curso 3", 'Curso 4']);
 });
 
 app.post('/courses', (request, response) => {
+    const body = request.body;
+    console.log(body);
     return response.json(['Curso 1', 'Curso 2', "Curso 3", 'Curso 4']);
 });
 
 app.put('/courses/:id', (request, response) => {
+    const { id } = request.params;
+    console.log(id);
     return response.json(['Curso 666', 'Curso 2', "Curso 3", 'Curso 4']);
 });
 
